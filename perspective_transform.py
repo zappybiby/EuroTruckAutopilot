63df70b669b9dd435235ef65d9c18a17a26cf0c7
zappybiby <blittle65@gmail.com>
2018-04-18 17:11:38 -0400

    re-calculated IPM
    still not all that great

--- perspective_transform.py
+++ perspective_transform.py
@@ -1,19 +1,16 @@
 import cv2
 import numpy as np
-
+import keyboard
 import image_process
 
-a = 20
-b = 60
+a = 920
+b = -160
 c = 70
 d = -70
 
 
 def perspective_transform(window):
-    global a
-    global b
-    global c
-    global d
+    global a, b, c, d
 
     window_resized = cv2.resize(window, (640, 360))
     original_resized = window_resized.copy()
@@ -31,30 +28,31 @@ def perspective_transform(window):
     original_size = (width_original, height_original)
 
     # Cut my life into pieces. This is my last resort.
-    # if keyboard.is_pressed('t'):
-    #     a += 10
-    #     print("a:", a)
-    # if keyboard.is_pressed('y'):
-    #     b += 10
-    #     print("b:", b)
-    # if keyboard.is_pressed('u'):
-    #     c += 10
-    #     print("c:", c)
-    # if keyboard.is_pressed('i'):
-    #     d += 10
-    #     print("d:", d)
-    # if keyboard.is_pressed('g'):
-    #     a -= 10
-    #     print("a", a)
-    # if keyboard.is_pressed('h'):
-    #     b -= 10
-    #     print("b:", b)
-    # if keyboard.is_pressed('j'):
-    #     c -= 10
-    #     print("c:", c)
-    # if keyboard.is_pressed('k'):
-    #     d -= 10
-    #     print("d:", d)
+    if keyboard.is_pressed('t'):
+        a += 100
+        print("a:", a)
+    if keyboard.is_pressed('y'):
+        b += 100
+        print("b:", b)
+    if keyboard.is_pressed('u'):
+        c += 10
+        print("c:", c)
+    if keyboard.is_pressed('i'):
+        d += 10
+        print("d:", d)
+    if keyboard.is_pressed('g'):
+        a -= 100
+        print("a", a)
+    if keyboard.is_pressed('h'):
+        b -= 100
+        print("b:", b)
+    if keyboard.is_pressed('j'):
+        c -= 10
+        print("c:", c)
+    if keyboard.is_pressed('k'):
+        d -= 10
+        print("d:", d)
+
 
     src = np.float32(
         [[(-a), height - b],
@@ -72,9 +70,10 @@ def perspective_transform(window):
 
     warped = cv2.warpPerspective(cropped_window, m, window_size, flags=cv2.INTER_LINEAR)
     original_warped = cv2.warpPerspective(cropped_original, m, original_size, flags=cv2.INTER_LINEAR)
+
     cv2.namedWindow("original_warped", cv2.WINDOW_NORMAL)
-    cv2.resizeWindow("original_warped", 500, 500)
-    cv2.moveWindow("original_warped", -700, 0)
+    cv2.resizeWindow("original_warped", 450, 500)
+    cv2.moveWindow("original_warped", -1820, 300)
     cv2.imshow("original_warped", original_warped)
     cv2.waitKey(1)
 
