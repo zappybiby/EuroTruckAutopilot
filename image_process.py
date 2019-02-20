b92b8c976fd80cb2ec0820f339eecdb44c44f894
brian littlefield <blittle65@gmail.com>
2019-02-20 14:38:46 -0500

    testing img processing

--- image_process.py
+++ image_process.py
@@ -1,20 +1,39 @@
 import cv2
 import numpy as np
-
+import keyboard
 import hough_lines
 
-
+a=0
+b=0
 def pre_process(capture, warped, original):
+    global a, b
+    #blur = cv2.GaussianBlur(original, (5, 5), 0)
+    hsv = cv2.cvtColor(original, cv2.COLOR_BGR2HSV)
+    # remove road noise (gray)
+    mask = cv2.inRange(hsv, np.uint8([0,0,0]), np.uint8([179, 255, 70]))
+    mask_inv = cv2.bitwise_not(mask)
 
-    #blur = cv2.GaussianBlur(original, (15, 15), 0)
-    gray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
-    #adaptive_thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 23, -3)
-    edges = cv2.Canny(gray, 150, 200)
+    noise_remove = cv2.bitwise_and(hsv, hsv, mask = mask_inv)
+    gray = cv2.cvtColor(noise_remove, cv2.COLOR_BGR2GRAY)
+    equ = cv2.equalizeHist(gray)
+    #canny = cv2.Canny(equ, 200, 670)
 
+    # if keyboard.is_pressed('t'):
+    #     a += 10
+    #     print("a:", a)
+    # if keyboard.is_pressed('y'):
+    #     b += 10
+    #     print("b:", b)
+    # if keyboard.is_pressed('g'):
+    #     a -= 10
+    #     print("a", a)
+    # if keyboard.is_pressed('h'):
+    #     b -= 10
+    #     print("b:", b)
     cv2.namedWindow("threshold", cv2.WINDOW_NORMAL)
     cv2.resizeWindow("threshold", 450, 500)
     cv2.moveWindow("threshold", -940, 300)
-    cv2.imshow("threshold", edges)
+    cv2.imshow("threshold", equ)
     cv2.waitKey(1)
 
-    hough_lines.hough_lines(edges, original)
+    hough_lines.hough_lines(equ, original)
